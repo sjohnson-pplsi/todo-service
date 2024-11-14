776e2603fd01a9bb1920d87aef36aa42986094ca
Sean Johnson <seanjohnson@pplsi.com>
2024-11-13 21:37:23 -0700

    Restored turbopack.

--- clients/todo-app/next.config.ts
+++ clients/todo-app/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import { join } from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbo: {
+      root: join(__dirname, "../../"),
+    },
+  },
 };
 
 export default nextConfig;
